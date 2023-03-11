fe5bf3df508676dbe039406ff217eb61d27f89a4
josueventura07 <josueandresventura@gmail.com>
2023-03-11 11:38:36 -0400

    feat: version movil

---
+++ main.js
@@ -0,0 +1,20 @@
+const logo = document.querySelector('.logo_container')
+const mainName = document.querySelector('.name')
+const iconWhatsapp = document.querySelector('.whatsapp')
+const phone = document.querySelector('.phone')
+
+window.addEventListener('scroll', (e) => {
+    if(window.scrollY >= 80) {
+        logo.classList.add('hidden_logo')
+        mainName.classList.add('hidden_name')
+    } else {
+        logo.classList.remove('hidden_logo')
+        mainName.classList.remove('hidden_name')
+    }
+})
+
+iconWhatsapp.addEventListener('click', (e) => {
+    iconWhatsapp.classList.toggle('deploy_whatsapp')
+    phone.classList.toggle('deploy_phone')
+    
+})
